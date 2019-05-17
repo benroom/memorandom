d072f6680ad24694f5d7be7cbd8b554c477849a1
benroom <45122905+benroom@users.noreply.github.com>
2019-05-17 15:09:24 +0900

    Add files via upload

--- js/init.js
+++ js/init.js
@@ -4,6 +4,7 @@
     $('.sidenav').sidenav();
     $(".dropdown-trigger").dropdown();
     $('.scrollspy').scrollSpy();
+    $('.parallax').parallax();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
